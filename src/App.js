bd8d94fe55b2c0698ddd7e9e1e01f6fc77358cac
shubham-alavni <shubham.alavni@gmail.com>
2024-01-29 21:36:15 +0530

    Use of Router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const routerConfig = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
-rootElement.render(<AppLayout />);
+rootElement.render(<RouterProvider router={routerConfig} />);
